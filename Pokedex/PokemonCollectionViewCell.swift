a934b9f4b89cce52b5963419e47befbdf6f5fe61
Felix Barros <felix.barros@gmail.com>
2016-02-24 16:45:52 -0500

    Add rounded corners to cell elements

--- Pokedex/PokemonCollectionViewCell.swift
+++ Pokedex/PokemonCollectionViewCell.swift
@@ -14,8 +14,16 @@ class PokemonCollectionViewCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
+        
+        thumbnailImage.layer.cornerRadius = 6
+        nameLabel.layer.cornerRadius = 6
         
         nameLabel.text = self.pokemon.name.capitalizedString
         thumbnailImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
