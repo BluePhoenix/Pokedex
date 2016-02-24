4db9288e98080a3572be81a53fc05f70aa6f3129
Felix Barros <felix.barros@gmail.com>
2016-02-23 22:18:41 -0500

    Create Pokemon class with initializer

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Felix Barros on 2/23/16.
+//  Copyright © 2016 Bits That Matter. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        _name = name
+        _pokedexID = pokedexID
+    }
+}
