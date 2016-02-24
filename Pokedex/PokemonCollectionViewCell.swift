2d52d26eae6c0232dbde01da52275c2b79bf983b
Felix Barros <felix.barros@gmail.com>
2016-02-23 22:44:08 -0500

    Create PokemonCollectionViewCell

---
+++ Pokedex/PokemonCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Felix Barros on 2/23/16.
+//  Copyright © 2016 Bits That Matter. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalizedString
+        thumbnailImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+}
